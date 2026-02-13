862ab1ab2b7a7605bb5eb8a850a9c49d471083cb
Nick Cao <nickcao@nichi.co>
2026-02-13 10:23:06 -0500

    Add torchao

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 git submodule update --init --recursive
 uv sync
+uv pip install --no-build-isolation -e ./thirdparty/executorch --verbose
+uv pip install --no-build-isolation -e ./thirdparty/optimum-executorch
